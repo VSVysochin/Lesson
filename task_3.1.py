8b9bd38d3817dd880cb72f13893551aa986aea4f
VSVysochin <86284845+VSVysochin@users.noreply.github.com>
2021-07-10 16:57:20 +0700

    Add files via upload

---
+++ task_3.1.py
@@ -0,0 +1,8 @@
+percent = int(input("Введите число от 1 до 20: "))
+
+if percent == 1:
+    print(percent, "процент")
+elif percent > 1 and percent < 5:
+    print(percent, "процента")
+else:
+    print(percent, "процентов")
